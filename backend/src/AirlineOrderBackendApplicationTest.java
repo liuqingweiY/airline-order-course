2d276402aad2893182c83e58534c7971fe550fe8
liuqingweiY <37173034+liuqingweiY@users.noreply.github.com>
2025-07-30 19:48:20 +0800

    Add files via upload

---
+++ backend/src/AirlineOrderBackendApplicationTest.java
@@ -0,0 +1,13 @@
+package airlineorderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AirlineOrderBackendApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
