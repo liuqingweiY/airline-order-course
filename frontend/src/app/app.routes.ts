374f04ab5707b79907e52f93c0dd49ba4864ff49
liuqingweiY <37173034+liuqingweiY@users.noreply.github.com>
2025-08-15 14:21:39 +0800

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -22,6 +22,6 @@ export const routes: Routes = [
     //canActivate: [authGuard]
   },
   
-  { path: '', redirectTo: '/orders', pathMatch: 'full' },
-  { path: '**', redirectTo: '/orders' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
 ];
